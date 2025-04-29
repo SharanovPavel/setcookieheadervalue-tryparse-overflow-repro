09d4b4954980e72874e4ff07495aacfa6d644292
SharanovPavel <sharanov.pasha@gmail.com>
2025-04-29 17:25:50 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Net.Http.Headers;
+
+// throws an exception
+var maxAgeOverflowedValue = 922337203686;
+SetCookieHeaderValue.TryParse($"sessionId=abc123; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age={maxAgeOverflowedValue}; Expires=Thu, 01 May 2025 12:00:00 GMT", out var _);
+
+// doesn't throw an exception
+var validMaxAgeValue = 922337203685;
+SetCookieHeaderValue.TryParse($"sessionId=abc123; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age={validMaxAgeValue}; Expires=Thu, 01 May 2025 12:00:00 GMT", out var _);
+
